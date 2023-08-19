b8ec295929845e9efab6603f1cfaf8cb3a1e9725
hmusinbego1 <hmusinbego1@etf.unsa.ba>
2023-08-19 02:19:02 +0200

    Controller part 10

--- src/main/java/ba/unsa/etf/rpr/controllers/components/DoubleButtonTableCell.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/components/DoubleButtonTableCell.java
@@ -1,4 +1,9 @@
 package ba.unsa.etf.rpr.controllers.components;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
 public class DoubleButtonTableCell {
+    public DoubleButtonTableCell(EventHandler<ActionEvent> buttonOne, EventHandler<ActionEvent> buttonTwo) {
+    }
 }
