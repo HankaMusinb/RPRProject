c4c989496be90b0688f2241c29b579e15b93376d
hmusinbego1 <hmusinbego1@etf.unsa.ba>
2023-08-19 02:19:48 +0200

    Controller part 12

--- src/main/java/ba/unsa/etf/rpr/controllers/components/DoubleButtonCellFactory.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/components/DoubleButtonCellFactory.java
@@ -28,6 +28,6 @@ public class DoubleButtonCellFactory<T> implements Callback<TableColumn<T, T>, T
 
     @Override
     public TableCell<T, T> call(TableColumn<T, T> quoteObjectTableColumn) {
-        return new DoubleButtonTableCell(buttonOne, buttonTwo);
+        return new DoubleButtonTableCell<>(buttonOne, buttonTwo);
     }
 }
