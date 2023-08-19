f4ee63238a717604c0841e59d7b68fdb397e4865
hmusinbego1 <hmusinbego1@etf.unsa.ba>
2023-08-19 02:21:36 +0200

    Controller part 13

--- src/main/java/ba/unsa/etf/rpr/controllers/ArtikliController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ArtikliController.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.ArtikliManager;
+
+/**
+ * Controller for managing Articles Entity
+ *
+ * @author Hanka Musinbegovic
+ */
 public class ArtikliController {
+    //managers
+    private final ArtikliManager artikliManager = new ArtikliManager();
+
+    //helper
 }
