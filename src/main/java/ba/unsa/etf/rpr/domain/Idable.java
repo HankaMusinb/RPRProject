3816c34889313f84dba3d0b93891551cc52faee7
hmusinbego1 <hmusinbego1@etf.unsa.ba>
2023-08-19 02:02:22 +0200

    //

--- src/main/java/ba/unsa/etf/rpr/domain/Idable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -2,6 +2,8 @@ package ba.unsa.etf.rpr.domain;
 
 /**
  * Interface that forces all POJO beans to have ID field. Name is stupid but per standard
+ *
+ * @author Hanka Musinbegovic
  */
 
 public interface Idable {
