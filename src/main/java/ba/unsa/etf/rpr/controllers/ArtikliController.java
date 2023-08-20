a82666aeaa3c63bbbce9b9cea71e713269703beb
hmusinbego1 <hmusinbego1@etf.unsa.ba>
2023-08-20 02:21:47 +0200

    Artikli controller

--- src/main/java/ba/unsa/etf/rpr/controllers/ArtikliController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ArtikliController.java
@@ -1,10 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.ArtikliManager;
+import ba.unsa.etf.rpr.domain.Artikli;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+
+import java.util.Date;
 
 /**
  * Controller for managing Articles Entity
@@ -21,7 +25,18 @@ public class ArtikliController {
 
     //components
 
-    public TableView quotesTable;
+    public TableView ArtikliTable;
+
     public TextField search;
+
+    public TableColumn<Artikli, String> idArtikli;
+
+    public TableColumn<Artikli, String> artikliColumn;
+
+    public TableColumn<Artikli, Date> createdColumn;
+
+    public TableColumn<Artikli, Integer> actionColumn;
+
+
 
 }
