82670c798ed75f8ec1d39580556896e7a210e373
hmusinbego1 <hmusinbego1@etf.unsa.ba>
2023-08-20 02:41:44 +0200

    AppFX project

---
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
