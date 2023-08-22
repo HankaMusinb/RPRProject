eb9e2921328a7e939f474c09d2d894965ba1bb9b
hmusinbego1 <hmusinbego1@etf.unsa.ba>
2023-08-23 00:47:39 +0200

    Ispis kategorija radi!

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,7 +1,7 @@
 package ba.unsa.etf.rpr;
 
 import ba.unsa.etf.rpr.dao.DaoFactory;
-import ba.unsa.etf.rpr.domain.Artikli;
+import ba.unsa.etf.rpr.domain.Kategorije;
 import ba.unsa.etf.rpr.exceptions.ArtikliException;
 import org.apache.commons.cli.Option;
 
@@ -19,13 +19,13 @@ public class App
      * Defining final variables to describe all code having options
      */
 
-    private static final org.apache.commons.cli.Option addQuote = new Option("artikal","dodaj-artikal",false, "Adding new article");
+    private static final org.apache.commons.cli.Option addArtikal = new Option("artikal","dodaj-artikal",false, "Adding new article");
 
 
     private static final Option addCategory = new Option("c","add-category",false, "Adding new category");
 
 
-    private static final Option getArtikal = new Option("getA", "get-articles",false, "Printing all articles");
+    private static final Option getArtikle = new Option("getA", "get-articles",false, "Printing all articles");
 
 
     private static final Option getCategories = new Option("getC", "get-categories",false, "Printing all categories");
@@ -35,14 +35,15 @@ public class App
 //         // ArtikliDao artikliDao = DaoFactory.artikliDao();
 //        System.out.println(kategorijeDao.getById(2));
         try {
-            List<Artikli> listaArtikala = new ArrayList<Artikli>(DaoFactory.artikliDao().getAll());
-            System.out.println(listaArtikala);
+            List<Kategorije> listaKategorija = new ArrayList<Kategorije>(DaoFactory.kategorijeDao().getAll());
+            System.out.println(listaKategorija);
+
         } catch (ArtikliException e) {
-            System.out.println("Nesto nije u redu sa getId() metodom!");
+            System.out.println("Nesto nije u redu sa getAll metodom!");
             throw new RuntimeException(e);
 
         }
-        System.out.println( "Hello World!" );
+       // System.out.println( "Hello World!" );
 
     }
 }
