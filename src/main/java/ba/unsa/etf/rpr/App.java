7635f8124aa77eb446fddccb1a09a0549121aaeb
hmusinbego1 <hmusinbego1@etf.unsa.ba>
2023-08-28 02:40:50 +0200

    Promjena Appp

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -2,7 +2,6 @@ package ba.unsa.etf.rpr;
 
 import ba.unsa.etf.rpr.business.ArtikliManager;
 import ba.unsa.etf.rpr.business.KategorijeManager;
-import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Artikli;
 import ba.unsa.etf.rpr.domain.Kategorije;
 import ba.unsa.etf.rpr.domain.Prodaje;
@@ -12,9 +11,7 @@ import org.apache.commons.cli.*;
 import java.io.PrintWriter;
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
-
 /**
  * Hello world!
  *
@@ -70,17 +67,17 @@ public class App
 
 
     public static void main( String[] args ) throws ArtikliException, ParseException {
-       //        KategorijeDao kategorijeDao = DaoFactory.kategorijeDao();
+   /**    //        KategorijeDao kategorijeDao = DaoFactory.kategorijeDao();
         //         // ArtikliDao artikliDao = DaoFactory.artikliDao();
         //        System.out.println(kategorijeDao.getById(2));
        try {
-       //  Prodaje zaradamesa = new Prodaje();
+        /// Prodaje zaradamesa = new Prodaje();
 
-       //     zaradamesa.setZarada(50);
-        //   DaoFactory.prodajeDao().add(zaradamesa);
+            //zaradamesa.setZarada(50);
+      //     DaoFactory.prodajeDao().add(zaradamesa);
            //-------------------------------------------------------
-          List<Prodaje> listaKategorija = new ArrayList<Prodaje>(DaoFactory.prodajeDao().getAll());
-           System.out.println(listaKategorija);
+      //    List<Artikli> listaKategorija = new ArrayList<Artikli>(DaoFactory.artikliDao().getAll());
+        //   System.out.println(listaKategorija);
            // Radi ispis prodaja
            //--------------------------------------------------------------------------------
 
@@ -99,12 +96,12 @@ public class App
             System.out.println("Nesto nije u redu sa add metodom!");
             throw new RuntimeException(e);
 
-        }
+        }*/
        // System.out.println( "Hello World!" );
 
         Options options = addOptions();
 
-        CommandLineParser commandLineParser = new DefaultParser(); //<- This may be the problem
+        GnuParser commandLineParser = new GnuParser();//<- This may be the problem
 
         CommandLine cl = commandLineParser.parse(options, args);
 
@@ -113,7 +110,7 @@ public class App
             KategorijeManager kategorijeManager = new KategorijeManager();
             Kategorije kategorije = null;
             try{
-                kategorije = searchThroughCategories(kategorijeManager.getAll(), cl.getArgList().get(1));
+                kategorije = searchThroughCategories(kategorijeManager.getAll(), (String) cl.getArgList().get(1));
 
             }catch (Exception e){
                 System.out.println("There is no category in the list! Try again.");
@@ -121,7 +118,7 @@ public class App
             }
         Artikli artikal = new Artikli();
         artikal.setKategorija(kategorije);
-        artikal.setNaziv(cl.getArgList().get(0));
+        artikal.setNaziv((String) cl.getArgList().get(0));
         artikal.setIstekRoka(Date.valueOf(LocalDate.now()));
         artikal.setCijena(5);
         Prodaje prodaja = new Prodaje();
@@ -131,17 +128,17 @@ public class App
         System.out.println("You successfully added artikal to database!");
     } else if(cl.hasOption(getArtikle.getOpt()) || cl.hasOption(getArtikle.getLongOpt())){
     ArtikliManager artikliManager = new ArtikliManager();
-    try {
-        artikliManager.getAll().forEach(q -> System.out.println(q.getNaziv()));
-    } catch (ArtikliException e) {
-        throw new RuntimeException(e);
-    }
+            try {
+                artikliManager.getAll().forEach(q -> System.out.println(q.getNaziv()));
+             } catch (ArtikliException e) {
+                throw new RuntimeException(e);
+             }
         //                break;
     } else if(cl.hasOption(addCategory.getOpt()) || cl.hasOption(addCategory.getLongOpt())){
     try {
         KategorijeManager kategorijeManager = new KategorijeManager();
         Kategorije cat = new Kategorije();
-        cat.setKategorija(cl.getArgList().get(0));
+        cat.setKategorija((String) cl.getArgList().get(0));
         kategorijeManager.add(cat);
         System.out.println("Category has been added successfully");
         //                    break;
